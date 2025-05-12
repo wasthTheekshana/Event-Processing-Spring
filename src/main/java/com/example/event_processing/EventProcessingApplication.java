2a886997f67d30ccb5ebabcc7b15ca88d469c76b
Theekshana Hariscandra <harishjonemike30@gmail.com>
2025-05-12 12:08:46 +0530

    Set up project and add readme.md

---
+++ src/main/java/com/example/event_processing/EventProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.event_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/event_processing/EventProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.event_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
